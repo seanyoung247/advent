ddd4383345741cfe39d8060242fb823cca979e90
Sean Young <seanyoung247@gmail.com>
2021-12-07 19:45:44 +0000

    feat: Improves day7 solution

--- js/solutions/day7.mjs
+++ js/solutions/day7.mjs
@@ -3,54 +3,25 @@ function formatData(input) {
     return input[0].split(',').map(Number);
 }
 
+function median(arr) {
+    const middle = Math.floor(arr.length / 2);
+    arr = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? arr[middle] : (arr[middle - 1] + arr[middle]) / 2;
+}
 export class Solutions {
     one(input) {
-        let range = 0;
-        let cost = {fuel: 0, value: -1};
         const crabs = formatData(input);
-        
-        // Find range
-        for (const crab of crabs) {
-            range = (crab > range) ? crab : range;
-        }
-
-        cost.fuel = range * crabs.length;
-        // Brute force for lowest cost
-        for (let i = 0; i < range + 1; i++) {
-            let fuel = 0;
-            for (const crab of crabs) {
-                fuel += Math.abs(crab - i);
-            }
-            if (fuel < cost.fuel) {
-                cost.fuel = fuel;
-                cost.value = i;
-            }
-        }
-
-        return cost.fuel;
+        // Calculate median average of all positions
+        const position = median(crabs);
+        // Calculate fuel cost for position
+        return crabs.reduce((t,c) => t += Math.abs(c - position), 0);
     }
 
     two(input) {
-        let position = 0;
-        let fuel = 0;
         const crabs = formatData(input);
-        
-        for (const crab of crabs) {
-            position += crab;
-        }
-        position = Math.round((position / crabs.length) - 0.1);
-
-        // Calculate Fuel Cost
-        for (const crab of crabs) {
-            let distance = Math.abs(crab - position);
-            let run = 1;
-            while (distance) {
-                fuel += run;
-                run++;
-                distance--;
-            }
-        }
-
-        return fuel;
+        // Calculate the mean average of all positions
+        const position = Math.round(crabs.reduce((t,c) => t += c) / crabs.length - 0.1);
+        // Calculate fuel cost for position
+        return crabs.reduce((t,c) => t += Math.abs(c - position) * ((Math.abs(c - position) + 1) / 2), 0);
     }
 }
