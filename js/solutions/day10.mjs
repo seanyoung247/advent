184d8a2a20893bd478a249299778d86675213f41
seanyoung247 <seanyoung247@gmail.com>
2021-12-10 01:34:37 +0000

    feat: Initial day 10

---
+++ js/solutions/day10.mjs
@@ -0,0 +1,16 @@
+
+// HELPER CODE HERE
+
+export class Solutions {
+    one(input) {
+        let result = 0;
+        // SOLUTION ONE CODE HERE
+        return result;
+    }
+
+    two(input) {
+        let result = 0;
+        // SOLUTION TWO CODE HERE
+        return result;
+    }
+}
