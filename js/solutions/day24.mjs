c0192ccf87d91bfbaf312e93cee91621812efa30
seanyoung247 <seanyoung247@gmail.com>
2021-12-21 16:41:05 +0000

    chore: Adds remaining challenge placeholders

---
+++ js/solutions/day24.mjs
@@ -0,0 +1,18 @@
+
+function formatData(input) {
+    
+}
+
+export class Solutions {
+    one(input) {
+        let result = 0;
+        // SOLUTION ONE CODE HERE
+        return result;
+    }
+
+    two(input) {
+        let result = 0;
+        // SOLUTION TWO CODE HERE
+        return result;
+    }
+}
