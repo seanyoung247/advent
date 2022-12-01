e264647fe3ea7a85f2733acf3522fc2ebe416f05
Sean Young <seanyoung247@gmail.com>
2022-12-01 15:57:42 +0000

    initial commit

--- js/lib.js
+++
@@ -1,9 +0,0 @@
-function toDecimal(binary) {
-    let result = 0;
-    let unit = 1;
-    for (let i = binary.length-1; i >= 0; i--) {
-        result += unit * parseInt(binary.charAt(i));
-        unit *= 2;
-    }
-    return result;
-}
