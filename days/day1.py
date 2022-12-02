2173cd103a683f7c59298efe7a86461f19f5c53d
sean <seanyoung247@gmail.com>
2022-12-02 23:11:41 +0000

    refactor: code layout

--- days/day1.py
+++ days/day1.py
@@ -1,16 +1,15 @@
 
 def format_data(data):
     elves = data.split('\n\n')
-    calories = [ sum([int(i) for i in elf.split('\n')]) for elf in elves] 
-    
+    calories = [ sum([int(i) for i in elf.split('\n')]) for elf in elves]
     return(calories)
 
 
 def solve_one(data):
-    return max(format_data(data)) 
+    return max(format_data(data))
 
 
 def solve_two(data):
     calories = format_data(data)
     calories.sort(reverse=True)
-    return sum( calories[0:3] )
+    return sum( calories[:3] )
