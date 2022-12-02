8172e5b2bd57ccbc07092d6c2e9a1d70cee780bf
sean <seanyoung247@gmail.com>
2022-12-02 16:42:44 +0000

    chore: Fixing git ignoring gitignore

---
+++ days/day2.py
@@ -0,0 +1,44 @@
+
+
+def format_data(data):
+    lines = data.split('\n')
+    for i in range(len(lines)):
+        lines[i] = lines[i].split(' ')
+
+    return lines
+
+
+get_choice = lambda row,col: ord(row[col]) - (65 + (23 * col))
+
+
+def solve_one(data):
+    combinations = [
+        [1,2,0],
+        [0,1,2],
+        [2,0,1] 
+    ]
+
+    data = format_data(data)
+    total = 0
+    for line in data:
+        outcome = combinations[get_choice(line,0)][get_choice(line,1)]
+        total += (outcome * 3) + get_choice(line, 1) + 1
+    
+    return total
+
+
+def solve_two(data):
+    combinations = [
+        [3,1,2],
+        [1,2,3],
+        [2,3,1]
+    ]
+
+    data = format_data(data)
+    total = 0
+    for line in data:
+        outcome = get_choice(line,1)
+        player2 = combinations[get_choice(line,0)][outcome]
+        total += (outcome * 3) + player2
+    
+    return total
