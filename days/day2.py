8ff6afcd27c0b3633ffbfce2d584531500736ac0
sean <seanyoung247@gmail.com>
2022-12-02 22:59:56 +0000

    docs: Adds code commenting

--- days/day2.py
+++ days/day2.py
@@ -7,10 +7,12 @@ get_choice = lambda row,col: ord(row[col]) - (65 + (23 * col))
 
 
 def solve_one(data):
+    # Matrix of game results. 1 = draw, 2 = win, 0 = loss
+        #R,P,S = Rock, Paper, Scissors
     combinations = [
-        [1,2,0],
-        [0,1,2],
-        [2,0,1] 
+        [1,2,0],    # Rock
+        [0,1,2],    # Paper
+        [2,0,1]     # Scissors
     ]
 
     data = format_data(data)
@@ -18,15 +20,17 @@ def solve_one(data):
     for line in data:
         outcome = combinations[get_choice(line,0)][get_choice(line,1)]
         total += (outcome * 3) + get_choice(line, 1) + 1
-    
+
     return total
 
 
 def solve_two(data):
+    # Matrix of choice to get desired result. 1 = Rock, 2 = Paper, 3 = Scissors
+        #L,D,W = Lose, Draw, Win
     combinations = [
-        [3,1,2],
-        [1,2,3],
-        [2,3,1]
+        [3,1,2],    # Rock
+        [1,2,3],    # Paper
+        [2,3,1]     # Scissors
     ]
 
     data = format_data(data)
@@ -35,5 +39,5 @@ def solve_two(data):
         outcome = get_choice(line,1)
         player2 = combinations[get_choice(line,0)][outcome]
         total += (outcome * 3) + player2
-    
+
     return total
