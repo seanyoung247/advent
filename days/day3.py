9a074b2992cdc4cbf40d79add517a439606902c0
sean <seanyoung247@gmail.com>
2022-12-03 09:45:10 +0000

    refactor: Code formatting

--- days/day3.py
+++ days/day3.py
@@ -14,18 +14,24 @@ def solve_one(data):
         bag = [ set(bag[:len(bag)//2]), set(bag[len(bag)//2:]) ]
         # For each item in first bag compartment
         for item in bag[0]:
-            # Find if it appears in the second compartment
+            # Find if item appears in the second compartment
             if item in bag[1]:
                 total += get_priority(item)
+
     return total
 
 
 def solve_two(data):
     bags = format_data(data)
     total = 0
+    # For every group of three bags
     for i in range(0, len(bags), 3):
+        # Ignore duplicate items
         bag = set(bags[i])
         for item in bag:
+            # If the item appears in both other bags
             if item in bags[i+1] and item in bags[i+2]:
+                # It is the badge
                 total += get_priority(item)
+
     return total
