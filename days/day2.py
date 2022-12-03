5690214f404fdb45e05e0ba4bb2edbedf2dfbb69
sean <seanyoung247@gmail.com>
2022-12-03 13:33:03 +0000

    refactor: compacts day2 code

--- days/day2.py
+++ days/day2.py
@@ -3,41 +3,20 @@ def format_data(data):
     return [ [ col for col in line.split(' ') ] for line in data.split('\n') ]
 
 
-get_choice = lambda row,col: ord(row[col]) - (65 + (23 * col))
+get_val = lambda row,col: ord(row[col]) - (65 + (23 * col))
 
 
 def solve_one(data):
-    # Matrix of game results. 1 = draw, 2 = win, 0 = loss
-        #R,P,S = Rock, Paper, Scissors
-    combinations = [
-        [1,2,0],    # Rock
-        [0,1,2],    # Paper
-        [2,0,1]     # Scissors
-    ]
-
-    data = format_data(data)
-    total = 0
-    for line in data:
-        outcome = combinations[get_choice(line,0)][get_choice(line,1)]
-        total += (outcome * 3) + get_choice(line, 1) + 1
-
-    return total
+    combinations = [[1,2,0],[0,1,2],[2,0,1]]
+    return sum([ 
+        (combinations[get_val(line,0)][get_val(line,1)] * 3) + get_val(line, 1) + 1 
+        for line in format_data(data) 
+    ])
 
 
 def solve_two(data):
-    # Matrix of choice to get desired result. 1 = Rock, 2 = Paper, 3 = Scissors
-        #L,D,W = Lose, Draw, Win
-    combinations = [
-        [3,1,2],    # Rock
-        [1,2,3],    # Paper
-        [2,3,1]     # Scissors
-    ]
-
-    data = format_data(data)
-    total = 0
-    for line in data:
-        outcome = get_choice(line,1)
-        player2 = combinations[get_choice(line,0)][outcome]
-        total += (outcome * 3) + player2
-
-    return total
+    combinations = [[3,1,2],[1,2,3],[2,3,1]]
+    return sum([
+        (get_val(line,1) * 3) + combinations[get_val(line,0)][get_val(line,1)]
+        for line in format_data(data) 
+    ])
