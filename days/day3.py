dc711d91e2710a11b604fdbbbbc2c564536e819e
sean <seanyoung247@gmail.com>
2022-12-03 12:07:04 +0000

    refactor: Improves code readability

--- days/day3.py
+++ days/day3.py
@@ -5,8 +5,22 @@ def format_data(data):
 get_priority = lambda ch : ord(ch) - (38 + (58 * ((ord(ch) & 32) >> 5)))
 
 def solve_one(data):
-    return sum([ sum([ get_priority(item) for item in set(bag[:len(bag)//2]) if item in bag[len(bag)//2:] ]) for bag in format_data(data) ])
+    return sum([
+        sum([
+            get_priority(item)
+            for item in set(bag[:len(bag)//2])
+            if item in bag[len(bag)//2:]
+
+        ]) for bag in format_data(data)
+    ])
 
 def solve_two(data):
     bags = format_data(data)
-    return sum([ sum( [ get_priority(item) for item in set(bags[i]) if item in bags[i+1] and item in bags[i+2] ] ) for i in range(0, len(bags), 3)])
+    return sum([
+        sum([
+            get_priority(item)
+            for item in set(bags[i])
+            if item in bags[i+1] and item in bags[i+2]
+            
+        ]) for i in range(0, len(bags), 3)
+    ])
