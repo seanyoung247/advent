6bb4a301035454e0471f592dbd0d87f88875a62f
sean <seanyoung247@gmail.com>
2022-12-04 11:13:22 +0000

    refactor: Refactors day 4 into one liners

--- days/day4.py
+++ days/day4.py
@@ -1,25 +1,18 @@
 
 def format_data(data):
-    return [ [ list(range(int(p.split('-')[0]),int(p.split('-')[1])+1)) for p in pair.split(',')] for pair in data.split('\n') ]
+    return  [ 
+        [
+            list(range(int(p.split('-')[0]), int(p.split('-')[1])+1)) 
+            for p in pair.split(',')
+        ]
+        for pair in data.split('\n') if pair 
+    ]
 
-subset = lambda a,b: a[0] in b and a[-1] in b
+subset_pair = lambda p: (p[0][0] in p[1] and p[0][-1] in p[1]) or (p[1][0] in p[0] and p[1][-1] in p[0])
 
 def solve_one(data):
-    data = format_data(data)
-    count = 0
-    for pair in data:
-        if subset(pair[0], pair[1]) or subset(pair[1], pair[0]):
-            count += 1
-
-    return count
+    return sum([1 for pair in format_data(data) if subset_pair(pair)])
 
 
 def solve_two(data):
-    data = format_data(data)
-    count = 0
-
-    for pair in data:
-        if any(item in pair[0] for item in pair[1]):
-            count += 1
-
-    return count
+    return sum([1 for pair in format_data(data) if any(item in pair[0] for item in pair[1]) ])
