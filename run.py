f6ad226e98bf3406667be24b875a263947dea9e4
sean <seanyoung247@gmail.com>
2022-12-04 12:35:02 +0000

    refactor: fixes pylint linting errors in run.py

--- run.py
+++ run.py
@@ -1,60 +1,63 @@
+""" Loads and runs code solutions for Advent of Code 2022 """
+
 import argparse
-from datetime import date 
+from datetime import date
 from importlib import import_module
 
 # Import solutions:
 solutions = []
 for i in range(1,25):
     try:
-        file = f'day{i}'
-        module = import_module(f'.{file}', package='days')
+        file_name = f'day{i}'
+        module = import_module(f'.{file_name}', package='days')
         solutions.append(module)
-    except:
+    except ModuleNotFoundError:
         break
 
 
 def load_data(file):
-    f = open(file)
-    data = f.read()
-    f.close()
-    return data
+    """ Loads solution data input file """
+    with open(file, encoding="utf-8") as data_file:
+        return data_file.read()
 
 
 def solve_day(day, data):
+    """ Runs given challenge solution """
     print('\tPart one: ', day.solve_one(data))
     print('\tPart two: ', day.solve_two(data))
 
 
 def main(args):
+    """ Runs all requested solutions """
     folder = 'tests' if args.test else 'data'
-    
     days = range(1,25) if args.all else [args.day]
-    
     for day in days:
         data_file = f'{folder}/day{day}.txt'
         try:
             solution = solutions[day-1]
-        except:
+        except IndexError:
             break
-
         print(f'Day {day}:')
         solve_day(solution, load_data(data_file))
-        
+
 
 if __name__ == '__main__':
-    # If it's currently december the default day should be the current one. Otherwise default to day 1
+    # If it's currently december the default day should be the current one.
+    # Otherwise default to day 1
     default_day = date.today().day if date.today().month == 12 else 1
 
     parser = argparse.ArgumentParser(description='Runs code solutions for Advent of Code 2022')
     parser.add_argument(
-        '-d', '--day', nargs='?', 
+        '-d', '--day', nargs='?',
         type=int, choices=range(1,26), metavar='[1-25]',
         const=default_day, default=default_day,
         help='Defines which day to solve.'
     )
-    parser.add_argument('-a', '--all', action='store_true', help='Runs all solutions in sequence')
-    parser.add_argument('-t', '--test', action='store_true', help='Uses test data instead of real data')
+    parser.add_argument(
+        '-a', '--all', action='store_true', help='Runs all solutions in sequence'
+    )
+    parser.add_argument(
+        '-t', '--test', action='store_true', help='Uses test data instead of real data'
+    )
 
-    args = parser.parse_args()
-
-    main(args)
+    main(parser.parse_args())
