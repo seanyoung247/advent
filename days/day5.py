94374b7dbf64477fa791d20d9e9509b22f9ed311
sean <seanyoung247@gmail.com>
2022-12-05 23:41:25 +0000

    refactor: Simplifies day 5 solution code

--- days/day5.py
+++ days/day5.py
@@ -9,7 +9,7 @@ def format_data(data):
     rows = len(sections[0]) - 1
 
     stacks = [
-        [sections[0][r][c*4+1:c*4+2] for r in range(rows)
+        [sections[0][r][c*4+1:c*4+2] for r in reversed(range(rows))
         if sections[0][r][c*4+1:c*4+2] != ' ']
         for c in range(cols)
     ]
@@ -25,26 +25,17 @@ def solve_one(data):
     """ Solves part one of day 5 """
     stacks,commands = format_data(data)
     for cmd in commands:
-        to_move = int(cmd[0])
-        from_stack = int(cmd[1])-1
-        to_stack = int(cmd[2])-1
-        for _ in range(to_move):
-            if len(stacks[from_stack]) != 0:
-                stacks[to_stack].insert(0, stacks[from_stack].pop(0))
+        stacks[cmd[2]-1].extend(reversed(stacks[cmd[1]-1][0 - cmd[0]:]))
+        del stacks[cmd[1]-1][0 - cmd[0]:]
 
-    return ''.join([ stack[0] for stack in stacks if len(stack) > 0])
-
+    return ''.join([ stack[-1] for stack in stacks if len(stack) > 0])
 
 
 def solve_two(data):
     """ Solves part two of day 5 """
     stacks,commands = format_data(data)
     for cmd in commands:
-        to_move = int(cmd[0])
-        from_stack = int(cmd[1])-1
-        to_stack = int(cmd[2])-1
-        for i in reversed(range(to_move)):
-            if len(stacks[from_stack]) > i:
-                stacks[to_stack].insert(0, stacks[from_stack].pop(i))
+        stacks[cmd[2]-1].extend(stacks[cmd[1]-1][0 - cmd[0]:])
+        del stacks[cmd[1]-1][0 - cmd[0]:]
 
-    return ''.join([ stack[0] for stack in stacks if len(stack) > 0])
+    return ''.join([ stack[-1] for stack in stacks if len(stack) > 0])
