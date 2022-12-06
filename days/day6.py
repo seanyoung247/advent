9cc0bd6e734919c295cf51b0fb1da9515602a2c2
sean <seanyoung247@gmail.com>
2022-12-06 08:58:54 +0000

    feat: Adds day 6 solution

---
+++ days/day6.py
@@ -0,0 +1,21 @@
+""" Solves challenge for day 6 of AoC 2022 """
+
+def solve(data, step):
+    """
+    Finds first group of characters of step length in data that have no
+    repeated characters and returns the index of the last character in the group
+    """
+    for i in range(len(data) - step):
+        if len(set(data[i:i + step])) == len(data[i:i + step]):
+            return i + step
+    return None
+
+
+def solve_one(data):
+    """ Solves part one of day 6 """
+    return solve(data, 4)
+
+
+def solve_two(data):
+    """ Solves part two of day 6 """
+    return solve(data, 14)
