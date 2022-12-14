84c46c8b2ecdb55d3d291986d36253ed129665bd
sean <seanyoung247@gmail.com>
2022-12-14 10:43:03 +0000

    feat: intial day 14 code

--- days/day14.py
+++ days/day14.py
@@ -1,4 +1,5 @@
 """ Solves challenge for day 14 of AoC 2022 """
+from .libs.coords import Coords
 
 # Generate coord runs
 # Add them as a set of uniques.
@@ -7,10 +8,17 @@
 
 def format_data(data):
     """ pre-formats data string"""
+    coords = set()
+    for line in data.split('\n'):
+        coords = line.split(' -> ')
+        for start,end in zip(coords,coords[1:]):
+            
 
+            
 
 def solve_one(data):
     """ Solves part one of day 14 """
+    format_data(data)
 
 
 def solve_two(data):
