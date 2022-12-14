7a0831414e8dbcb42ffa0e30ef6de3982c13bac0
Sean Young <seanyoung247@gmail.com>
2022-12-14 10:42:21 +0000

    refactor: Moves coord class to seperate lib

--- days/day9.py
+++ days/day9.py
@@ -1,32 +1,5 @@
 """ Solves challenge for day 9 of AoC 2022 """
-
-class Coord:
-    """ Defines a single 2D coordinate """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        return Coord(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Coord(self.x - other.x, self.y - other.y)
-
-    def __hash__(self):
-        return hash((self.x,self.y))
-
-    def __eq__(self, other):
-        if not isinstance(other, type(self)):
-            return False
-        return (self.x == other.x and self.y == other.y)
-
-    def direction(self):
-        """ Returns a normalised direction vector for this coord """
-        return Coord(
-            (self.x > 0) - (self.x < 0),
-            (self.y > 0) - (self.y < 0)
-        )
-
+from .libs.coord import Coord
 
 def format_data(data):
     """ pre-formats data string"""
