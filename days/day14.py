bbe9e90127d0c94cd3033467d742315593dc81f7
Sean Young <seanyoung247@gmail.com>
2022-12-14 08:55:26 +0000

    feat: Day 14 initial commit

---
+++ days/day14.py
@@ -0,0 +1,17 @@
+""" Solves challenge for day 14 of AoC 2022 """
+
+# Generate coord runs
+# Add them as a set of uniques.
+# Each cycle move sand down. Check set. 
+# If coord exists try left, try right, or append sand to set.
+
+def format_data(data):
+    """ pre-formats data string"""
+
+
+def solve_one(data):
+    """ Solves part one of day 14 """
+
+
+def solve_two(data):
+    """ Solves part two of day 14 """
